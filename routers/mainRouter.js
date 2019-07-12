ab7572a3bf5907db2170f8908c885fc0b982dce2
gogogoghost <491511934@qq.com>
2019-07-12 12:42:14 +0800

    header

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -4,12 +4,12 @@ const key='6cbc2339-f02a-4dbb-9087-b0bcc12ed229';
 
 const router=new Router();
 router.post('/post-receive',async(ctx)=>{
-    if(ctx.request.body&&ctx.request.headers['X-Gitee-Token']==key){
+    if(ctx.request.headers['X-Gitee-Token']==key){
         ctx.response.status=200;
     }else{
         ctx.response.status=403;
     }
-    //console.log(ctx.request.body);
+    console.log(ctx.request.headers);
 });
 
 module.exports=router.routes();
