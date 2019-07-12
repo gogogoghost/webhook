8425e142bd0a0e5d9330a5032408c3c2a7488833
gogogoghost <491511934@qq.com>
2019-07-12 12:38:45 +0800

    body

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -4,12 +4,12 @@ const key='6cbc2339-f02a-4dbb-9087-b0bcc12ed229';
 
 const router=new Router();
 router.post('/post-receive',async(ctx)=>{
-    if(ctx.body&&ctx.body.key==key){
+    if(ctx.request.body&&ctx.request.body.key==key){
         ctx.response.status=200;
     }else{
         ctx.response.status=403;
     }
-    console.log(ctx.body);
+    console.log(ctx.request.body);
 });
 
 module.exports=router.routes();
