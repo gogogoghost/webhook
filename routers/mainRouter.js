3cfaa36d4749a30592f5dc189a151af838ce585d
gogogoghost <491511934@qq.com>
2019-07-12 12:06:03 +0800

    post

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -3,7 +3,7 @@ const Router=require('koa-router');
 const key='6cbc2339-f02a-4dbb-9087-b0bcc12ed229';
 
 const router=new Router();
-router.get('/post-receive',async(ctx)=>{
+router.post('/post-receive',async(ctx)=>{
     if(ctx.body&&ctx.body.key==key){
         ctx.response.status=200;
     }else{
